0ee23de631f1f4bbe8da5aab7618c5d54d58a4e0
Krunoslav Blazicevic <123366059+noKru1986@users.noreply.github.com>
2023-03-06 20:20:49 +0100

    my first app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, useCallback } from "react";
+import "./App.css";
 
 function App() {
+  const [godina, setGodina] = useState(2023);
+  const [klik, setKlik] = useState(0);
+
+  const init = useCallback(() => {
+    console.log("component init");
+  }, []);
+
+  useEffect(() => {
+    init();
+  }, [init]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <p>{godina}</p>
+      <button
+        onClick={() => {
+          setGodina(godina + 1);
+        }}
+      >
+        DODAJ BROJ
+      </button>
+      <p>{godina}</p>
+      <button
+        onClick={() => {
+          setGodina(godina + 1);
+        }}
+      >
+        DODAJ KLIK
+      </button>
     </div>
   );
 }
